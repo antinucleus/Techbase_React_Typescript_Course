694fc22f95915912dccddc31f623e7bd108627c0
antinucleus <antinucleuss@gmail.com>
2022-05-12 18:45:02 +0300

    3-UseState episode completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { User } from './interfaces';
+
 
 function App() {
+  const [user, setUser] = useState<User | null>(null)
+
+  const fetchUser = () => setUser({
+    name: "Faruk",
+    age: 23,
+    country: "Turkey",
+    address: {
+      street: "Istanbul",
+      number: 1453,
+      zip: "1071"
+    },
+    admin: true
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={fetchUser} >Fetch user on click</button>
+      {user && <p>{`Welcome ${user.name}`}</p>}
+    </>
   );
 }
 
