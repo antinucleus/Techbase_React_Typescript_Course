470f3e9e9f7b4bb728bebc179dc0377024bd51d2
antinucleus <antinucleuss@gmail.com>
2022-05-13 00:22:13 +0300

    8-Narrowing with react episode completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Component, { Item } from "./components/Component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const items: Item[] = [
+    {
+      id: 1,
+      type: "imageItem",
+      title: "A nice sunset",
+      imageUrl:
+        "https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80",
+    },
+    {
+      id: 2,
+      type: "quoteItem",
+      quote:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, quam ea. Nisi nulla earum itaque, sapiente exercitationem, laudantium sunt fuga dolores repellendus, expedita dicta. Voluptates minima laboriosam odit reprehenderit magnam!",
+    },
+  ];
+
+  return <Component items={items} />;
 }
-
-export default App;
