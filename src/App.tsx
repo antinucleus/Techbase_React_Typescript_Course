988b14e267bf9cd05ef6d42fd430b95de52e4b15
antinucleus <antinucleuss@gmail.com>
2022-05-12 21:20:21 +0300

    5-Context api episode completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from "react";
+import { InputValueContext } from "./context/InpuTvalueContext";
 
-function App() {
+export default function App() {
+  const { state, dispatch } = useContext(InputValueContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Value: {state.inputValue}</p>
+      <button onClick={() => dispatch({ type: "SET_INPUT_VALUE_TO_100" })}>
+        SET VALUE TO 100
+      </button>
+    </>
   );
 }
-
-export default App;
