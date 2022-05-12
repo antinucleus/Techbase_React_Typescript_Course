8d87780a367af23d93af2e4ed01425d53b691aaf
antinucleus <antinucleuss@gmail.com>
2022-05-12 18:42:44 +0300

    2-Props episode completed

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App headerText='This is a header text' extraText='some extra text' />
   </React.StrictMode>
 );
 
